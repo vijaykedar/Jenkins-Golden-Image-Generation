a0db8ceed7d9bb82e6ef217b3d0b183a671bcbb3
Vijay Kedar <64856368+vijaykedar@users.noreply.github.com>
2025-06-11 09:39:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,21 +5,17 @@
 
 #!/bin/bash
 efs=$1
+
 echo "#----------------------#"
 echo "-- Install ansible --"
 echo "#----------------------#"
-apt-get update -y
-apt-get install ansible -y
+sudo apt update
+sudo apt install -y ansible
 
 cd /home/ubuntu
 echo "#-------------------------------------#"
 echo "-- Extracting ansible scripts --"
 echo "#-------------------------------------#"
-if [ ! -f jenkinsrole.tar ];
-then
-  echo "ERROR: jenkinsrole.tar not found!"
-  exit 1
-fi
 tar -xvf jenkinsrole.tar
 
 echo "#------------------------------------------------------------#"
@@ -30,7 +26,7 @@ ansible-playbook jenkins.yml -e "efs=$efs"
 echo "#----------------------#"
 echo "-- Remove ansible --"
 echo "#----------------------#"
-sudo apt-get remove -y ansible
+sudo apt remove -y ansible
 
 #---------------------------------------------#
 # Author: Adam WezvaTechnologies
